3c52cac0ff0e051364e790e3efe96b9e0b69e8fc
luisleandro1995 <84648700+luisleandro1995@users.noreply.github.com>
2024-10-02 15:19:10 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'pacman-cache-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/service-worker.js',
+    '/images/pacman.png', // If you have images
+    '/images/ghost.png',  // If you have images
+];
+
+// Install Service Worker and cache assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Archivos en cache');
+                return cache.addAll(assetsToCache);
+            })
+    );
+});
+
+// Fetch cached assets when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate Service Worker and remove old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (!cacheWhitelist.includes(key)) {
+                        console.log('Cache viejo eliminado:', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
